ed9bdc307ca3794a6fa27f1690348b6e42d46d40
SAlA-9 <SAlA-9@SI9-8.salas.local>
2024-09-30 09:26:34 -0500

    Hola bebe donde esta acabo de aterrizar en mede YIN

---
+++ pintegragod/src/main/java/com/mycompany/pintegragod/Pintegragod.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pintegragod;
+
+/**
+ *
+ * @author SAlA-9
+ */
+public class Pintegragod {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
